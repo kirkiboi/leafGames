67e05927fc98bcd85a95c9f91e4d9f056673af70
RZ_cola <168092735+kirkiboi@users.noreply.github.com>
2025-06-22 11:48:20 +0800

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,21 @@
+// const navigationAbout = document.getElementById("nav-about");
+// const navigationGames = document.getElementById("nav-games");
+// const navigationNews = document.getElementById("nav-news");
+// const navigationContact = document.getElementById("nav-contact");
+
+// navigationAbout.addEventListener ('click', ()=>{
+//     const aboutUsSection = document.querySelector(".about-us-section");
+//     aboutUsSection.scrollIntoView({behavior: "smooth"});
+// });
+// navigationGames.addEventListener ('click', ()=>{
+//     const gamesSection = document.querySelector(".games-section");
+//     gamesSection.scrollIntoView({behavior: "smooth"});
+// });
+// navigationNews.addEventListener ('click', ()=>{
+//     const newsSection = document.querySelector(".news-section");
+//     newsSection.scrollIntoView({behavior: "smooth"});
+// });
+// navigationContact.addEventListener ('click', ()=>{
+//     const contactSection = document.querySelector(".contacts");
+//     contactSection.scrollIntoView({behavior: "smooth"});
+// });
